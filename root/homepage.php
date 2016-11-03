94cfb79af4929621458fda103b823e896d878f39
Byonnem <menno.spitteler@gmail.com>
2016-11-03 11:35:36 +0100

    homepage.php aangemaakt
    v1 van de homepage met include php

---
+++ root/homepage.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title> POP Jan van Egmond </title>
+		<link rel="stylesheet" type="text/css" href="theme.css">
+	</head>
+	<body>
+	<h1> Welkom op deze POP pagina </h1>
+		<div class="login">
+			<?php include 'login.php';?>
+		</div>
+	</body>
+</html>
